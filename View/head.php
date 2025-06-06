ca24b6cef6a5fb7b79faab3a319a13dc0b2b9af9
Ferchoch0 <delvalle.fernando.d@gmail.com>
2025-06-06 10:25:02 -0300

    v0.1: front de servicios agregada

---
+++ View/head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Services</title>
+
+    <script src="https://code.iconify.design/iconify-icon/1.0.8/iconify-icon.min.js"></script>
+    <link rel="stylesheet" href="assets/css/main.css">
---
+++ View/home.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once 'head.php'
+
+?>
+
+<link rel="stylesheet" href="assets/css/home.css">
+
+
+</head>
+
+<body>
+
+    <header>
+        <nav>
+            <article class="nav-logo">
+                <a href="">
+                    <img src="../public/logo.svg" alt="Logo">
+                </a>
+            </article>
+        </nav>
+    </header>
+
+    <main>
+        <section class="services-container">
+            <h1>¿Que servicio vas a realizar?</h1>
+            <p>Selecciona el servicio que deseas realizar y te guiaremos en el proceso.</p>
+            <article class="services-items">
+                <a href="">
+                    <span><iconify-icon icon="mdi:bug" width="24" height="24"></iconify-icon></span>
+                    <h2>Control de plagas</h2>
+                </a>
+                <a href="">
+                    <span><iconify-icon icon="mdi:air-conditioner" width="24" height="24"></iconify-icon></span>
+                    <h2>Aire acondicionado</h2>
+                </a>
+                <a href=""> 
+                    <span><iconify-icon icon="mdi:fan" width="24" height="24"></iconify-icon></span>
+                    <h2>Limpieza ductos de ventilación</h2>
+                </a>
+                <a href="">
+                    <span><iconify-icon icon="mdi:water" width="24" height="24"></iconify-icon></span>
+                    <h2>Limpieza y desinfeccion de tanques de agua</h2>
+                </a>
+                <a href="">
+                    <span><iconify-icon icon="mdi:stove" width="24" height="24"></iconify-icon></span>
+                    <h2>Limpieza de campana y ductos de cocina</h2>
+                </a>
+            </article>
+
+        </section>
+    </main>
+
+    <footer class="footer">
+        <p>&copy; 2023 Our Services. All rights reserved.</p>
+    </footer>
+</body>
+
+</html>
